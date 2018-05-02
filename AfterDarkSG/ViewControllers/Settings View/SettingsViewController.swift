397cd53e7349882e3aaf404ebcef5bac8bb12cf7
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2018-05-02 13:12:39 +0800

    added more tab

---
+++ AfterDarkSG/ViewControllers/Settings View/SettingsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SettingsViewController.swift
+//  AfterDarkSG
+//
+//  Created by Koh Yi Zhi Elliot - Ezekiel on 2/5/18.
+//  Copyright © 2018 Kohbroco. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        self.tableView.reloadData()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
